681117f86451ca3a84769331d3fbfa965f6add34
isopets <jandp.0717@gmail.com>
2025-11-24 20:10:42 +0900

    fix: Remove unsupported backupFileExtension option

--- home.nix
+++ home.nix
@@ -1,20 +1,27 @@
 { config, pkgs, ... }:
 {
+  # 基本設定
   home.enableNixpkgsReleaseCheck = false;
   home.username = "isogaiyuto";
   home.homeDirectory = "/Users/isogaiyuto";
-  home.backupFileExtension = "backup";     # ★重要: バックアップ設定
-  services.kdeconnect.enable = false;      # ★重要: クラッシュ回避
 
+  # クラッシュ回避
+  services.kdeconnect.enable = false;
+
+  # パッケージリスト
   home.packages = with pkgs; [
     eza bat lazygit fzf direnv jq snyk gnupg mise
   ];
 
+  # Zsh設定
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     initExtra = "source ~/dotfiles/zsh/.zshrc"; 
   };
+  
   programs.git.enable = true;
+  
+  # バージョンロック
   home.stateVersion = "23.11";
 }
