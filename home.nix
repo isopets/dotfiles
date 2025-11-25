ee9501cbdef519e8448381bfbf87c6728507e94e
isopets <jandp.0717@gmail.com>
2025-11-25 21:38:42 +0900

    fix: Disable backup option to force install

--- home.nix
+++ home.nix
@@ -1,19 +1,36 @@
 { config, pkgs, ... }:
+
 {
+  # バージョン不一致警告を無視
   home.enableNixpkgsReleaseCheck = false;
   home.username = "isogaiyuto";
   home.homeDirectory = "/Users/isogaiyuto";
-  home.backupFileExtension = "backup";
+
+  # ★自動バックアップ設定を削除 (これがエラー原因)
+  # home.backupFileExtension = "backup";
+
+  # クラッシュ回避
   services.kdeconnect.enable = false;
+
+  # パッケージリスト
   home.packages = with pkgs; [
     eza bat lazygit fzf direnv starship mise
-    jq gnused ripgrep fd gnupg snyk trivy
+    jq gnused ripgrep fd gnupg
+    snyk trivy
     (nerdfonts.override { fonts = [ "Hack" ]; })
     gum
   ];
-  programs.zsh.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.initExtra = "source ~/dotfiles/zsh/.zshrc";
+
+  # Zsh設定
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    initExtra = "source ~/dotfiles/zsh/.zshrc";
+    envExtra = "export PATH=$HOME/.nix-profile/bin:$PATH";
+  };
+
   programs.git.enable = true;
+  
+  # バージョンロック
   home.stateVersion = "24.05";
 }
