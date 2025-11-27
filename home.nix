1682a204e42f51ff449353d5dfdfaca36a8cda41
isopets <jandp.0717@gmail.com>
2025-11-27 21:58:32 +0900

    fix: Remove backup option to fix installation error

--- home.nix
+++ home.nix
@@ -1,59 +1,35 @@
 { config, pkgs, ... }:
 
 {
-  # --- Imports ---
-  imports = [
-    ./nix/pkgs.nix  # パッケージリストを読み込む
-  ];
-
-  # --- Basic Configuration ---
   home.enableNixpkgsReleaseCheck = false;
   home.username = "isogaiyuto";
   home.homeDirectory = "/Users/isogaiyuto";
-  
-  # 衝突時の自動バックアップ (24.05で有効)
-  home.backupFileExtension = "backup";
 
-  # macOSでのクラッシュ回避
+  # ★ここを削除しました (もう手動退避するので不要)
+  # home.backupFileExtension = "backup";
+
   services.kdeconnect.enable = false;
+  
+  home.packages = with pkgs; [
+    eza bat lazygit fzf direnv starship mise
+    jq gnused ripgrep fd gnupg
+    snyk trivy gum
+    (nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
 
-  # --- Font Configuration ---
   fonts.fontconfig.enable = true;
 
-  # --- Starship Configuration ---
-  # 設定は ./config/starship.toml に記述し、リンクを貼る
+  # Starship設定 (外部ファイル)
   programs.starship.enable = true;
   xdg.configFile."starship.toml".source = ./config/starship.toml;
 
-  # --- Zsh Configuration ---
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    
-    # Dotfilesの .zshrc を読み込む
-    initExtra = ''
-      source ~/dotfiles/zsh/.zshrc
-    '';
-    
-    # Nixのパスを優先的に通す
-    envExtra = ''
-      export PATH=$HOME/.nix-profile/bin:$PATH
-    '';
+    initExtra = "source ~/dotfiles/zsh/.zshrc";
+    envExtra = "export PATH=$HOME/.nix-profile/bin:$PATH";
   };
 
-  # --- Git Configuration ---
-  programs.git = {
-    enable = true;
-    userName = "isopets";
-    userEmail = "jandp.0717@gmail.com";
-    
-    # 便利なエイリアスや設定があればここに追加可能
-    extraConfig = {
-      init.defaultBranch = "main";
-      core.editor = "code --wait";
-    };
-  };
-
-  # --- Version Lock ---
+  programs.git.enable = true;
   home.stateVersion = "24.05";
 }
