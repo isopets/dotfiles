499c7ecfd07bae0c1854ae00e180530f853e83bf
isopets <jandp.0717@gmail.com>
2025-11-29 21:21:23 +0900

    config: Update config (modules)

--- home.nix
+++ home.nix
@@ -1,15 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # 既存の pkgs.nix をインポート (パッケージリストの定義)
-  imports = [ ./nix/pkgs.nix ];
-
-  # 全てのモジュールをインポート (設定の上書き、結合)
-  modules = [
-  ./nix/modules/core.nix
-  ./nix/modules/shell.nix
-  ./nix/modules/vscode.nix
-  ./nix/modules/vscode/profiles.nix
+ # 既存の imports を拡張し、全てのモジュールを含める
+  imports = [ 
+    # パッケージリスト
+    ./nix/pkgs.nix
+    
+    # 全ての設定モジュール
+    ./nix/modules/core.nix
+    ./nix/modules/shell.nix
+    ./nix/modules/vscode.nix
+    ./nix/modules/vscode/profiles.nix 
   ];
 
   # ★ここを削除しました (もう手動退避するので不要)
