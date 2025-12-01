ed843e0f1ab4728802efebc0dc470c2b75257a53
isopets <jandp.0717@gmail.com>
2025-12-01 22:35:49 +0900

    chore(nix): update configuration

--- nix/modules/shell.nix
+++ nix/modules/shell.nix
@@ -1,39 +1,49 @@
 { config, pkgs, ... }:
 
 {
-  # --- Tools & Integrations ---
-  # ※ Zsh本体の設定は zsh.nix に移動済み
+  # --- 1. Magical History (Atuin) ---
+  programs.atuin = {
+    enable = true;
+    enableZshIntegration = true;
+    flags = [ "--disable-up-arrow" ]; # Ctrl+R で起動
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      search_mode = "fuzzy";
+      style = "compact";
+    };
+  };
 
-  # Direnv (環境切り替え)
+  # --- 2. Core Tools ---
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
   };
 
-  # Mise (言語バージョン管理)
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
   };
   
-  # Zoxide (高速移動)
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
     options = ["--cmd cd"];
   };
 
-  # Git
   programs.git = {
     enable = true;
     userName = "isopets";
     userEmail = "jandp.0717@gmail.com";
   };
 
-  # Starship (プロンプト)
   programs.starship.enable = true;
   xdg.configFile."starship.toml".source = ../../config/starship.toml;
 
-  # Fonts
   fonts.fontconfig.enable = true;
+  
+  # --- 3. Extra Packages ---
+  home.packages = with pkgs; [
+    zsh-fzf-tab # 視覚的補完
+  ];
 }
